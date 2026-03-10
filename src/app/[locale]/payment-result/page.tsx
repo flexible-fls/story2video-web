c26cb000c1b0d0713f80114c120a5a01cdc0de49
flexible-fls <473628209@qq.com>
2026-03-11 02:18:07 +0800

    fix payment result page build error

--- src/app/[locale]/payment-result/page.tsx
+++ src/app/[locale]/payment-result/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { usePathname, useRouter } from "next/navigation";
+import Link from "next/link";
 import { supabase } from "@/lib/supabase";
 import LanguageSwitch from "@/components/LanguageSwitch";
 import BackButton from "@/components/BackButton";
